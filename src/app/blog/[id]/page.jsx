0d826b271e1e1a9f17b5def1433253123496742a
Esraa Soliman <ee341110@gmail.com>
2025-07-23 22:42:48 +0300

    fix errors

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -2,26 +2,18 @@ import Image from "next/image";
 import React from "react";
 import styles from "./page.module.css";
 
-async function getData(id) {
-  const res = await fetch(`http://localhost:3000/api/posts/${id}`, {
-    cache: "no-store",
-  });
+const staticData = {
+  title: "Sample Blog Post",
+  desc: "This is a sample description for the blog post.",
+  username: "JohnDoe",
+  content: "This is the content of the sample blog post.",
+  img: "https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg",
+};
 
-  if (!res.ok) {
-    return notFound();
-  }
+export default function BlogPost({ params }) {
+  const data = staticData;
 
-  return res.json();
-}
-
-export default async function BlogPost({ params }) {
-  const data = await getData(params.id);
-  const imageUrl =
-    data.img.startsWith("/") ||
-    data.img.startsWith("http://") ||
-    data.img.startsWith("https://")
-      ? data.img
-      : ``;
+  const imageUrl = data.img;
 
   return (
     <div className={styles.container}>
@@ -31,23 +23,17 @@ export default async function BlogPost({ params }) {
           <p className={styles.desc}>{data.desc}</p>
           <div className={styles.author}>
             <Image
-              src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg"
+              src={data.img}
               alt=""
               width={40}
               height={40}
               className={styles.avatar}
             />
-
             <span className={styles.username}>{data.username}</span>
           </div>
         </div>
         <div className={styles.imageContainer}>
-          <Image
-            src={imageUrl}
-            alt=""
-            fill={true}
-            className={styles.image}
-          />
+          <Image src={imageUrl} alt="" fill={true} className={styles.image} />
         </div>
       </div>
       <div className={styles.content}>
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -3,37 +3,35 @@ import styles from "./page.module.css";
 import Image from "next/image";
 import Link from "next/link";
 
-async function getData() {
-  const res = await fetch("http://localhost:3000/api/posts", {
-    cache: "no-store",
-  });
+const staticData = [
+  {
+    _id: "1",
+    title: "First Blog Post",
+    desc: "This is the description for the first blog post.",
+    img: "https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg",
+  },
+  {
+    _id: "2",
+    title: "Second Blog Post",
+    desc: "This is the description for the second blog post.",
+    img: "https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg",
+  },
+];
 
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
-
-  return res.json();
-}
-
-export default async function Blog() {
-  const data = await getData();
+export default function Blog() {
+  const data = staticData;
 
   return (
     <div className={styles.container}>
       {data && data.length > 0 ? (
         data.map((item) => {
-          const imageUrl =
-            item.img.startsWith("/") ||
-            item.img.startsWith("http://") ||
-            item.img.startsWith("https://")
-              ? item.img
-              : ``;
+          const imageUrl = item.img;
 
           return (
             <Link
               href={`/blog/${item._id}`}
               className={styles.item}
-              key={item.id}
+              key={item._id}
             >
               <div className={styles.imgContainer}>
                 <Image src={imageUrl} alt="" fill={true} />
